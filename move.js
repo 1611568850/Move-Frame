170fbc7b6822315350bd2625a8a01889cd967847
1611568850 <1611568850@qq.com>
2020-08-11 11:58:12 +0800

    move 1.3 兼容opactiy变换

--- move.js
+++ move.js
@@ -2,18 +2,28 @@ function move(obj,name,target,fn){
 				clearInterval(obj.timer)
 				// alert(parseInt(getComputedStyle(oDiv1).width))
 				obj.timer=setInterval(function(){
-					let nowStyle=parseInt(getComputedStyle(obj)[name])
-						let speed=(target-nowStyle)/10
-						if (speed>0) {
-							speed=Math.ceil(speed)
+						if (name=='opacity') {
+							nowStyle=getComputedStyle(obj)[name]*100
 						} else{
-							speed=Math.floor(speed)
+							nowStyle=parseInt(getComputedStyle(obj)[name])
 						}
+						// if (speed>0) {
+						// 	speed=Math.ceil(speed)
+						// } else{
+						// 	speed=Math.floor(speed)
+						// }
+						let speed=(target-nowStyle)/10
+						speed=(speed>0) ? Math.ceil(speed) : speed=Math.floor(speed)
+						
 					if (nowStyle==target) {
 						clearInterval(obj.timer)
 						if(fn){fn()}
 					} else{
-						obj.style[name]=nowStyle+speed+'px'
+						if (name=='opacity') {
+							obj.style[name]=(nowStyle+speed)/100;
+						} else{
+							obj.style[name]=nowStyle+speed+'px'
+						}
 					}
 				},10)
 			}
