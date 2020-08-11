9bfe7b1f6939558301bbd53c595c1751bc857984
1611568850 <1611568850@qq.com>
2020-08-11 10:52:22 +0800

    move1.1版本 解决speed强制取整归零

--- move.js
+++ move.js
@@ -3,10 +3,15 @@ function move(obj,name,target){
 				// alert(parseInt(getComputedStyle(oDiv1).width))
 				obj.timer=setInterval(function(){
 					let nowStyle=parseInt(getComputedStyle(obj)[name])
+						let speed=(target-nowStyle)/10
+						if (speed>0) {
+							speed=Math.ceil(speed)
+						} else{
+							speed=Math.floor(speed)
+						}
 					if (nowStyle==target) {
 						clearInterval(obj.timer)
 					} else{
-						let speed=(target-nowStyle)/10
 						obj.style[name]=nowStyle+speed+'px'
 					}
 				},10)
